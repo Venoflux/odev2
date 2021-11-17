63fc7d534ff01b73da534c73400b58b21e0ec4ec
Venoflux <erolsarpk@gmail.com>
2021-11-17 20:12:43 +0100

    Koleksiyonlar-Soru-3

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle giriniz: ");
+
+            string input = Console.ReadLine();
+            List<char> letters = new List<char>();
+
+            foreach (var letter in input)
+                if ("aeioöuüAEIİOÖUÜ".Contains(letter))
+                    letters.Add(letter);
+
+            Console.WriteLine("Girdiğiniz cümledeki bütün sesli harfler. ");
+            
+            foreach (var answer in letters)
+                Console.WriteLine(answer);
+        }
+    }
+}
